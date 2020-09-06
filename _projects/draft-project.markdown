c466b40d34c2ec169f0251d35a62537e4b74d5d3
terryzzzzz <ter2y@Ter2ys-MBP.modem>
2020-09-06 23:50:35 +1000

    update single project page template

--- _projects/draft-project.markdown
+++ _projects/draft-project.markdown
@@ -1,8 +1,9 @@
 ---
 layout: page-single-project
-title:  "Draft Project"
-date:   2020-06-20 23:25:15 +1000
+title: "Draft Project -<br />A Delicious Soft Drink"
+date: 2020-06-20 23:25:15 +1000
 categories: jekyll update
 hero_bg: "/assets/images/draft/hero-background.jpg"
 ---
+
 Hello world
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,84 @@
-$(document).ready(function(){
+$(document).ready(function () {
   // init
-    init();
+  init();
 
-    // Start ------ Page Transition
-    const options = {
-      animationSelector: '[class*="transition-fade"]',
-      animateHistoryBrowsing: true,
-      plugins: [
-        new SwupScrollPlugin({
-            animateScroll: false
-        })
-      ]
-    };
-    const swup = new Swup(options);
-    let scrollValues = {};
-    swup.on('clickLink', () => {
-        scrollValues[window.location.href] = window.scrollY;
-    });
-    swup.on('popState', () => {
-        setTimeout(function() {
-            window.scrollTo(0, scrollValues[window.location.href]);
-        }, 100);
-    });
-    swup.on('contentReplaced', init);
-    // End ------ Page Transition
+  // Start ------ Page Transition
+  const options = {
+    animationSelector: '[class*="transition-fade"]',
+    animateHistoryBrowsing: true,
+    plugins: [
+      new SwupScrollPlugin({
+        animateScroll: false,
+      }),
+    ],
+  };
+  const swup = new Swup(options);
+  let scrollValues = {};
+  swup.on("clickLink", () => {
+    scrollValues[window.location.href] = window.scrollY;
+  });
+  swup.on("popState", () => {
+    setTimeout(function () {
+      window.scrollTo(0, scrollValues[window.location.href]);
+    }, 100);
+  });
+  swup.on("contentReplaced", init);
+  // End ------ Page Transition
 
+  // Start ------ Sticky Header
+  let siteDesktopHeader = $(".site-header");
+  let headerHeight = siteDesktopHeader.outerHeight();
+  $(window).scroll(function () {
+    let offset = $(document).scrollTop();
+    if (offset > 0) {
+      siteDesktopHeader
+        .removeClass("bg-transparent py-8")
+        .addClass("bg-gray-800 py-4");
+    } else {
+      siteDesktopHeader
+        .addClass("bg-transparent py-8")
+        .removeClass("bg-gray-800 py-4");
+    }
+  });
+  // End ------ Sticky Header
 
   // Functions
-    function init() {
-      // GSAP For Type Animation
-      let gridTl = gsap.timeline();
-      gridTl.from('.grid-yellow', {duration: 2, height: 0, ease: "circ.out", delay: 1})
+  function init() {
+    // GSAP For Type Animation
+    let gridTl = gsap.timeline();
+    gridTl.from(".grid-yellow", {
+      duration: 2,
+      height: 0,
+      ease: "circ.out",
+      delay: 1,
+    });
 
-
-      const words = ["Terry.", "A Web Developer.", "A Graphic Designer.", "A Quick Learner."]
-      let boxTl = gsap.timeline()
-      boxTl.to('.hi', {duration: 0.5, opacity: 1, delay: 0.2, ease: "power3.in", onComplete: () => masterTl.play()})
-      .to('.cursor', {opacity: 1, ease: "power2.inOut", repeat: -1})
-      let masterTl = gsap.timeline({repeat: -1}).pause()
-      words.forEach(word => {
-        let tl = gsap.timeline({repeat: 1, yoyo: true, repeatDelay: 1, delay: 1})
-        tl.to('.text', {duration: 1, text: word})
-        masterTl.add(tl)
+    const words = [
+      "Terry.",
+      "A Web Developer.",
+      "A Graphic Designer.",
+      "A Quick Learner.",
+    ];
+    let boxTl = gsap.timeline();
+    boxTl
+      .to(".hi", {
+        duration: 0.5,
+        opacity: 1,
+        delay: 0.2,
+        ease: "power3.in",
+        onComplete: () => masterTl.play(),
       })
-    }
-})
+      .to(".cursor", { opacity: 1, ease: "power2.inOut", repeat: -1 });
+    let masterTl = gsap.timeline({ repeat: -1 }).pause();
+    words.forEach((word) => {
+      let tl = gsap.timeline({
+        repeat: 1,
+        yoyo: true,
+        repeatDelay: 1,
+        delay: 1,
+      });
+      tl.to(".text", { duration: 1, text: word });
+      masterTl.add(tl);
+    });
+  }
+});
